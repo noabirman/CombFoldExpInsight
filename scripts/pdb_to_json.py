14793a654f7282d2fbf214b781f15eb5ac56c5f4
tsori <tsori.kislev@mail.huji.acil>
2024-12-02 14:55:58 +0200

    update merged_subunits

--- scripts/pdb_to_json.py
+++ scripts/pdb_to_json.py
@@ -1,13 +1,39 @@
 import os
 import sys
 from typing import Dict
-import libs.utils_classes
 from run_on_pdbs import get_chain_to_seq
-from libs.utils_classes import SubunitInfo, save_subunits_info  # Make sure SubunitInfo is available
+from libs.utils_classes import SubunitInfo, save_subunits_info, SubunitsInfo  # Make sure SubunitInfo is available
 from Bio.PDB import PDBParser, PDBIO
+import re
+from typing import List, Tuple
 
 
-def create_subunit_info_from_chain_seq(chain_to_seq: Dict[str, str]) -> Dict[str, SubunitInfo]:
+def get_first_non_x_index(sequence: str) -> int:
+    """
+    Get the index of the first non-'X' character in a sequence.
+
+    Args:
+        sequence (str): The sequence to search.
+
+    Returns:
+        int: The index of the first non-'X' character, or -1 if none is found.
+    """
+    for i, aa in enumerate(sequence):
+        if aa != 'X':
+            return i
+    return -1
+
+
+def create_subunit_info_from_chain_seq(chain_to_seq: Dict[str, str]) -> SubunitsInfo:
+    """
+    Create a SubunitsInfo object from a dictionary of chain sequences.
+
+    Args:
+        chain_to_seq (Dict[str, str]): A dictionary mapping chain IDs to sequences.
+
+    Returns:
+        SubunitsInfo: A dictionary mapping chain IDs to SubunitInfo objects.
+    """
     subunits_info = {}
 
     for chain_id, sequence in chain_to_seq.items():
@@ -33,13 +59,18 @@ def create_subunit_info_from_chain_seq(chain_to_seq: Dict[str, str]) -> Dict[str
     return subunits_info
 
 
-import re
-from typing import List, Tuple
+def find_long_x_sequences(sequence) -> List[Tuple[int, int]]:
+    """
+    Find all sequences of 'X' characters longer than 100 in a given text.
 
+    Args:
+        sequence (str): The text to search.
 
-def find_long_x_sequences(text) -> List[Tuple[int, int]]:
+    Returns:
+        List[Tuple[int, int]]: A list of tuples containing the start and end indices of each long 'X' sequence.
+    """
     # Find all sequences of 'X' characters
-    x_sequences = re.finditer(r'X+', text)
+    x_sequences = re.finditer(r'X+', sequence)
     # Filter for sequences with length greater than 100 and store their positions and lengths
     result = [(match.start(), match.end()) for match in x_sequences if
               (match.end() - match.start()) > 100]
@@ -48,39 +79,59 @@ def find_long_x_sequences(text) -> List[Tuple[int, int]]:
 
 def merge_exi_and_full_seq_into_subunit_info(experimental: Dict[str, str], full_seq: Dict[str, str]) -> Dict[
     str, SubunitInfo]:
+    """
+    Merge experimental sequences with full sequences to create SubunitInfo objects.
+
+    Args:
+        experimental (Dict[str, str]): A dictionary mapping chain IDs to experimental sequences.
+        full_seq (Dict[str, str]): A dictionary mapping chain IDs to full sequences.
+
+    Returns:
+        Dict[str, SubunitInfo]: A dictionary mapping subunit names to SubunitInfo objects.
+    """
     subunits_info = {}
     for chain_id, sequence in experimental.items():
-        missing_subunits = find_long_x_sequences(sequence)
+        missing_subunits = find_long_x_sequences(sequence) # Find long 'X' sequences
         if not missing_subunits:  # in case no holes, keep the original seq and chain_id
-            subunit_name = chain_id + "EXI"
-            subunits_info[subunit_name] = SubunitInfo(name=subunit_name, chain_names=[chain_id], start_res=1,
-                                                      sequence=sequence)
+            subunit_name = chain_id + "EXI" # EXI for existing
+            start_res_index = get_first_non_x_index(sequence) + 1 # base 1
+            subunits_info[subunit_name] = SubunitInfo(name=subunit_name, chain_names=[chain_id],
+                                                      start_res=start_res_index,
+                                                      sequence=sequence[start_res_index:])
+        # if there are holes, we need to create new subunits
         subunit_num = 1
         start_index = 0
         for hole in missing_subunits:
-            # before hole, if there is sequence:
-            if hole[0] != 0:
+            if hole[0] != 0:  # if there is a sequence before the hole
                 subunit_name = chain_id + str(subunit_num) + "EXI"
+                start_res_index = get_first_non_x_index(sequence[start_index:hole[0]]) + start_index + 1 # base 1
                 subunits_info[subunit_name] = SubunitInfo(name=subunit_name, chain_names=[chain_id],
-                                                          start_res=start_index + 1,  # base 1?
-                                                          sequence=sequence[start_index:hole[0]])
+                                                          start_res=start_res_index + 1,  # base 1
+                                                          sequence=sequence[start_res_index:hole[0]])
                 subunit_num += 1
             # filling hole:
             subunit_name = chain_id + str(subunit_num) + "MIS"
             subunits_info[subunit_name] = SubunitInfo(name=subunit_name, chain_names=[chain_id],
-                                                      start_res=hole[0] + 1,  # base 1?
+                                                      start_res=hole[0] + 1,  # base 1
                                                       sequence=full_seq[chain_id][hole[0]:hole[1]])
             subunit_num += 1
             if hole == missing_subunits[-1]:  # if we are in the last hole
                 if hole[1] < len(sequence):
                     subunit_name = chain_id + str(subunit_num) + "EXI"
                     subunits_info[subunit_name] = SubunitInfo(name=subunit_name, chain_names=[chain_id],
-                                                              start_res=hole[1] + 1,  # base 1?
+                                                              start_res=hole[1] + 1,  # base 1
                                                               sequence=sequence[hole[1]:])
+            start_index = hole[1]
     return subunits_info
 
 
 def set_b_factor_to_100(pdb_path: str):
+    """
+    Set the B-factor of all atoms in a PDB file to 100.
+
+    Args:
+        pdb_path (str): The path to the PDB file.
+    """
     # Parse the PDB file
     parser = PDBParser()
     pdb_id = os.path.splitext(os.path.basename(pdb_path))[0]
@@ -93,10 +144,18 @@ def set_b_factor_to_100(pdb_path: str):
     # Save the modified structure
     io = PDBIO()
     io.set_structure(structure)
-    io.save(pdb_id+"_b_factor_100.pdb")
+    io.save(pdb_id + "_b_factor_100.pdb")
 
 
 def update_b_factors_preserve_metadata(input_pdb: str, output_pdb: str, new_b_factor: float = 100.0) -> None:
+    """
+    Update the B-factors of atoms in a PDB file while preserving metadata.
+
+    Args:
+        input_pdb (str): The path to the input PDB file.
+        output_pdb (str): The path to the output PDB file.
+        new_b_factor (float, optional): The new B-factor value. Defaults to 100.0.
+    """
     with open(input_pdb, "r") as infile, open(output_pdb, "w") as outfile:
         for line in infile:
             if line.startswith("ATOM") or line.startswith("HETATM"):
@@ -106,6 +165,7 @@ def update_b_factors_preserve_metadata(input_pdb: str, output_pdb: str, new_b_fa
             else:
                 # Write metadata and other lines unchanged
                 outfile.write(line)
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
